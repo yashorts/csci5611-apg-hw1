60c2c940b989bdd430287f1a3b79c09b66a42a62
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-03 01:20:15 -0600

    removes processing project dirs

--- src/Fire.java
+++ src/Fire.java
@@ -3,70 +3,74 @@ import processing.core.PShape;
 
 import java.util.ArrayList;
 
+import queasycam.*;
+
 public class Fire extends PApplet {
+    QueasyCam cam;
 
-    class CameraState {
-        Vector3D eye;
-        Vector3D center;
-        Vector3D up;
-
-        CameraState(Vector3D eye, Vector3D center, Vector3D up) {
-            this.eye = eye;
-            this.center = center;
-            this.up = up;
-            apply();
-        }
-
-        public void apply() {
-            camera(
-                    eye.x, eye.y, eye.z,
-                    center.x, center.y, center.z,
-                    up.x, up.y, up.z
-            );
-        }
-    }
+//    class CameraState {
+//        Vector3D eye;
+//        Vector3D center;
+//        Vector3D up;
+//
+//        CameraState(Vector3D eye, Vector3D center, Vector3D up) {
+//            this.eye = eye;
+//            this.center = center;
+//            this.up = up;
+//            apply();
+//        }
+//
+//        public void apply() {
+//            camera(
+//                    eye.x, eye.y, eye.z,
+//                    center.x, center.y, center.z,
+//                    up.x, up.y, up.z
+//            );
+//        }
+//    }
 
     FireParticleSystem ps;
-    CameraState cs;
+    //    CameraState cs;
     PShape s;
 
     @Override
     public void setup() {
         ps = new FireParticleSystem(1);
-        cs = new CameraState(
-                new Vector3D(1000 / 2.0f, 700 / 2.0f, (700 / 2.0f) / tan(PI * 30.0f / 180.0f)),
-                new Vector3D(1000 / 2.0f, 700 / 2.0f, 0),
-                new Vector3D(0, 1, 0)
-        );
+//        cs = new CameraState(
+//                new Vector3D(1000 / 2.0f, 700 / 2.0f, (700 / 2.0f) / tan(PI * 30.0f / 180.0f)),
+//                new Vector3D(1000 / 2.0f, 700 / 2.0f, 0),
+//                new Vector3D(0, 1, 0)
+//        );
         noStroke();
+        cam = new QueasyCam(this);
         surface.setTitle("Processing");
         s = loadShape("BirchTree_1.obj");
         /* s.rotate(3.14, 0.0, 0.0, 1.0); */
         s.rotate(3.14f, 0.0f, 1.0f, 0.0f);
     }
 
-    public void moveCameraFromInput() {
-        Vector3D viewDir = cs.center.minus(cs.eye).unit();
-        Vector3D sideDir = new Vector3D(viewDir.z, 0, -viewDir.x);
-        if (keyPressed && keyCode == UP) {
-            cs.eye = cs.eye.plus(viewDir.scale(4));
-        } else if (keyPressed && keyCode == DOWN) {
-            cs.eye = cs.eye.plus(viewDir.scale(-4));
-        } else if (keyPressed && keyCode == LEFT) {
-            cs.eye = cs.eye.plus(sideDir.scale(-4));
-            s.rotate(0.1f, 0.0f, 1.0f, 0.0f);
-        } else if (keyPressed && keyCode == RIGHT) {
-            cs.eye = cs.eye.plus(sideDir.scale(4));
-            s.rotate(-0.1f, 0.0f, 1.0f, 0.0f);
-        }
-
-        cs.apply();
-    }
+//    public void moveCameraFromInput() {
+//        Vector3D viewDir = cs.center.minus(cs.eye).unit();
+//        Vector3D sideDir = new Vector3D(viewDir.z, 0, -viewDir.x);
+//        if (keyPressed && keyCode == UP) {
+//            cs.eye = cs.eye.plus(viewDir.scale(4));
+//        } else if (keyPressed && keyCode == DOWN) {
+//            cs.eye = cs.eye.plus(viewDir.scale(-4));
+//        } else if (keyPressed && keyCode == LEFT) {
+//            cs.eye = cs.eye.plus(sideDir.scale(-4));
+//            s.rotate(0.1f, 0.0f, 1.0f, 0.0f);
+//        } else if (keyPressed && keyCode == RIGHT) {
+//            cs.eye = cs.eye.plus(sideDir.scale(4));
+//            s.rotate(-0.1f, 0.0f, 1.0f, 0.0f);
+//        }
+//
+//        cs.apply();
+//    }
 
     @Override
     public void draw() {
         // camera
-        moveCameraFromInput();
+//        moveCameraFromInput();
         int frameStart = millis();
         // physics
         ps.update();
