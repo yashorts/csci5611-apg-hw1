33369c76adb7c8ef4a7601958f8d840b7c8c990b
Yashasvi Sriram <yash.3997@gmail.com>
2020-02-04 17:23:50 -0600

    improves code

--- src/FireParticle.java
+++ src/FireParticle.java
@@ -33,6 +33,7 @@ public class FireParticle {
         this.remainingLifespan = remainingLifespan;
         this.totalLifeSpan = remainingLifespan;
         this.texture = texture;
+        this.color = Vec3.of(0, 255, 0);
     }
 
     public void physics(float dt) {
@@ -56,15 +57,7 @@ public class FireParticle {
                 color = gradientColor();
                 // very small portion of initial particles turning into smog
                 if (parent.random(1) < 0.0001) {
-                    // smog particles come out of jet and slow down due to high air resistance
-                    velocity = velocity.scale(parent.random(1));
-                    // their lifespan is increased to show their effects
-                    remainingLifespan += 100;
-                    totalLifeSpan += 100;
-                    stage = Stage.SMOG;
-                    if (parent.random(1) < 0.1) {
-                        shape = Shape.BOX;
-                    }
+                    changeStageToSmog();
                 }
                 // jet stage ends after some lifespan
                 if (remainingLifespan / totalLifeSpan <= 0.6) {
@@ -76,15 +69,7 @@ public class FireParticle {
                 velocity = velocity.plus(acceleration.scale(dt));
                 color = gradientColor();
                 if (parent.random(1) < (0.54 - remainingLifespan / totalLifeSpan)) {
-                    // smog particles come out of jet and slow down due to high air resistance
-                    velocity = velocity.scale(parent.random(1));
-                    // their lifespan is increased to show their effects
-                    remainingLifespan += 100;
-                    totalLifeSpan += 100;
-                    stage = Stage.SMOG;
-                    if (parent.random(1) < 0.1) {
-                        shape = Shape.BOX;
-                    }
+                    changeStageToSmog();
                 } else {
 //                    float theta = parent.random(2 * parent.PI);
 //                    float radius = 0.5f * (float) Math.sqrt(parent.random(1));
@@ -107,15 +92,7 @@ public class FireParticle {
                 }
                 // small portion of ball particles turning into smog
                 if (parent.random(1) < 0.002) {
-                    // smog particles come out of jet and slow down due to high air resistance
-                    velocity = velocity.scale(parent.random(1));
-                    // their lifespan is increased to show their effects
-                    remainingLifespan += 100;
-                    totalLifeSpan += 100;
-                    stage = Stage.SMOG;
-                    if (parent.random(1) < 0.1) {
-                        shape = Shape.BOX;
-                    }
+                    changeStageToSmog();
                 }
                 break;
             case SMOG:
@@ -128,6 +105,8 @@ public class FireParticle {
                 break;
             case DEAD:
                 return;
+            default:
+                return;
         }
         remainingLifespan -= 1;
         if (remainingLifespan <= 0) {
@@ -136,78 +115,83 @@ public class FireParticle {
     }
 
     public void render() {
-        if (stage == Stage.JET) {
-            if (shape == Shape.BOX) {
-                parent.fill(color.x, color.y, color.z);
-                parent.stroke(color.x, color.y, color.z);
-                parent.pushMatrix();
-                parent.translate(position.x, position.y, position.z);
-
-                parent.rotate(remainingLifespan / totalLifeSpan * parent.PI * 20, 0, 1, 0);
-                parent.beginShape();
-                parent.texture(texture);
-                float sideLen = 0.5f * (1.5f - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1);
-                parent.vertex(-sideLen, -sideLen, 0, 0, 0);
-                parent.vertex(sideLen, -sideLen, 0, texture.width, 0);
-                parent.vertex(sideLen, sideLen, 0, texture.width, texture.height);
-                parent.vertex(-sideLen, sideLen, 0, 0, texture.height);
-                parent.endShape();
-
-                parent.popMatrix();
-            } else {
-                parent.stroke(color.x, color.y, color.z);
-                parent.point(position.x, position.y, position.z);
-            }
-        } else if (stage == Stage.BALL) {
-            if (shape == Shape.BOX) {
-                parent.fill(color.x, color.y, color.z);
-                parent.stroke(color.x, color.y, color.z);
-                parent.pushMatrix();
-                parent.translate(position.x, position.y, position.z);
-                parent.box(1f * (1 - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
-                parent.popMatrix();
-            } else {
-                parent.stroke(color.x, color.y, color.z);
-                parent.point(position.x, position.y, position.z);
-            }
-        } else if (stage == Stage.SMOG) {
-            if (shape == Shape.BOX) {
-                parent.fill(color.x, color.y, color.z);
-                parent.stroke(color.x, color.y, color.z);
-                parent.pushMatrix();
-                parent.translate(position.x, position.y, position.z);
-                parent.box(0.75f * (1 - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
-                parent.popMatrix();
-            } else {
-                parent.stroke(color.x, color.y, color.z);
-                parent.point(position.x, position.y, position.z);
-            }
-        } else {
-            if (shape == Shape.BOX) {
-                parent.fill(color.x, color.y, color.z);
-                parent.stroke(color.x, color.y, color.z);
-                parent.pushMatrix();
-                parent.translate(position.x, position.y, position.z);
-
-                parent.rotate(remainingLifespan / totalLifeSpan * parent.PI * 20, 0, 1, 0);
-                parent.beginShape();
-                parent.texture(texture);
-                float sideLen = 0.5f * (1.5f - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1);
-                parent.vertex(-sideLen, -sideLen, 0, 0, 0);
-                parent.vertex(sideLen, -sideLen, 0, texture.width, 0);
-                parent.vertex(sideLen, sideLen, 0, texture.width, texture.height);
-                parent.vertex(-sideLen, sideLen, 0, 0, texture.height);
-                parent.endShape();
-
-                parent.popMatrix();
-            } else {
-                parent.stroke(color.x, color.y, color.z);
-                parent.point(position.x, position.y, position.z);
-            }
+        switch (stage) {
+            case JET:
+                if (shape == Shape.BOX) {
+                    renderQuad(0.4f * (1.5f - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
+                } else {
+                    renderPoint();
+                }
+                break;
+            case BALL:
+                if (shape == Shape.BOX) {
+                    renderBox(1f * (1 - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
+                } else {
+                    renderPoint();
+                }
+                break;
+            case SMOG:
+                if (shape == Shape.BOX) {
+                    renderBox(0.75f * (1 - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
+                } else {
+                    renderPoint();
+                }
+                break;
+            default:
+                if (shape == Shape.BOX) {
+                    renderQuad(0.5f * (1.5f - remainingLifespan / totalLifeSpan) * Math.min(acceleration.abs(), 1));
+                } else {
+                    renderPoint();
+                }
+                break;
         }
     }
 
     private Vec3 gradientColor() {
         return Vec3.of(255, 255 * (remainingLifespan / totalLifeSpan), 255 * Math.max(2 * remainingLifespan / totalLifeSpan - 1, 0));
+    }
+
+    private void changeStageToSmog() {
+        // smog particles come out of jet and slow down due to high air resistance
+        velocity = velocity.scale(parent.random(1));
+        // their lifespan is increased to show their effects
+        remainingLifespan += 100;
+        totalLifeSpan += 100;
+        stage = Stage.SMOG;
+        if (parent.random(1) < 0.1) {
+            shape = Shape.BOX;
+        }
+    }
+
+    private void renderBox(float size) {
+        parent.fill(color.x, color.y, color.z);
+        parent.stroke(color.x, color.y, color.z);
+        parent.pushMatrix();
+        parent.translate(position.x, position.y, position.z);
+        parent.box(size);
+        parent.popMatrix();
+    }
+
+    private void renderQuad(float sideLen) {
+        parent.fill(color.x, color.y, color.z);
+        parent.stroke(color.x, color.y, color.z);
+        parent.pushMatrix();
+
+        parent.translate(position.x, position.y, position.z);
+        parent.rotate(remainingLifespan / totalLifeSpan * parent.PI * 20, 0, 1, 0);
+        parent.beginShape();
+        parent.texture(texture);
+        parent.vertex(-sideLen, -sideLen, 0, 0, 0);
+        parent.vertex(sideLen, -sideLen, 0, texture.width, 0);
+        parent.vertex(sideLen, sideLen, 0, texture.width, texture.height);
+        parent.vertex(-sideLen, sideLen, 0, 0, texture.height);
+        parent.endShape();
+
+        parent.popMatrix();
+    }
+
+    private void renderPoint() {
+        parent.stroke(color.x, color.y, color.z);
+        parent.point(position.x, position.y, position.z);
     }
 }
